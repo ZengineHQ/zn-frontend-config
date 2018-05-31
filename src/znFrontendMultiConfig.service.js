63b130c8f4e259f86432fd1cb684707660e986ca
Alex Weber <alexweber15@gmail.com>
2018-05-31 09:53:55 -0300

    style: coding standards

--- src/znFrontendMultiConfig.service.js
+++ src/znFrontendMultiConfig.service.js
@@ -1,105 +1,104 @@
-plugin.service('wgnMultiConfigSrv', ['$q', '$firebase', 'znData',
-	function ($q, $firebase, znData) {
+plugin.service('wgnMultiConfigSrv', ['$q', '$firebase', 'znData', function ($q, $firebase, znData) {
 
-		var srv = this;
+	var srv = this;
 
-		/**
-		 * Loads plugins data.
-		 *
-		 * @return {Promise<Object>} Plugin data.
-		 */
-		var loadPlugin = function () {
-			var q = $q.defer();
+	/**
+	 * Loads plugins data.
+	 *
+	 * @return {Promise<Object>} Plugin data.
+	 */
+	var loadPlugin = function () {
+		var q = $q.defer();
 
-			znData('Plugins').get({
-					namespace: 'wgn'
-				},
-				function (resp) {
-					q.resolve(resp[0]);
-				},
-				function (resp) {
-					q.reject(resp);
-				}
-			);
-
-			return q.promise;
-		};
-
-		/**
-		 * Connects to Firebase and retrieves plugins settings for this workspace.
-		 *
-		 * @param {Object} plugin The plugins data.
-		 * @param {number} workspaceId The workspace id.
-		 *
-		 * @return {Promise<Object>} Plugin settings.
-		 */
-		var connect = function (plugin, workspaceId) {
-			var q = $q.defer();
-			var ref = new Firebase(plugin.firebaseUrl + workspaceId + '/settings');
-
-			ref.auth(plugin.firebaseAuthToken, function (err) {
-				if (err) {
-					q.reject(err);
-				}
-				var sync = $firebase(ref).$asArray();
-				sync.$loaded().then(function (settings) {
-					q.resolve(settings);
-				});
-			});
-
-			return q.promise;
-		};
-
-		/**
-		 * Connects to Firebase and retrieves plugins settings for this workspace.
-		 *
-		 * @param {number} workspaceId The workspace id.
-		 *
-		 * @return {Promise<Object>} Plugin settings.
-		 */
-		srv.load = function (workspaceId) {
-			return loadPlugin().then(function (plugin) {
-				return connect(plugin, workspaceId);
-			});
-		};
-
-		/**
-		 * Saves plugins settings for this workspace.
-		 *
-		 * @param {number} workspaceId
-		 * @param {Object} settings
-		 * @param {Object} config
-		 *
-		 * @return {Promise<*>}
-		 */
-		srv.save = function (workspaceId, settings, config) {
-			if (config.$id) {
-				var index = settings.$indexFor(config.$id);
-				settings[index] = config;
-				return settings.$save(index);
+		znData('Plugins').get({
+				namespace: 'wgn'
+			},
+			function (resp) {
+				q.resolve(resp[0]);
+			},
+			function (resp) {
+				q.reject(resp);
 			}
+		);
 
-			return settings.$add(config);
-		};
+		return q.promise;
+	};
 
-		/**
-		 * Completely deletes a given configuration.
-		 *
-		 * @param {number} workspaceId
-		 * @param {Object} config
-		 * @param {Object} settings
-		 *
-		 * @return {Promise<*>}
-		 */
-		srv.deleteConfig = function (workspaceId, config, settings) {
+	/**
+	 * Connects to Firebase and retrieves plugins settings for this workspace.
+	 *
+	 * @param {Object} plugin The plugins data.
+	 * @param {number} workspaceId The workspace id.
+	 *
+	 * @return {Promise<Object>} Plugin settings.
+	 */
+	var connect = function (plugin, workspaceId) {
+		var q = $q.defer();
+		var ref = new Firebase(plugin.firebaseUrl + workspaceId + '/settings');
+
+		ref.auth(plugin.firebaseAuthToken, function (err) {
+			if (err) {
+				q.reject(err);
+			}
+			var sync = $firebase(ref).$asArray();
+			sync.$loaded().then(function (settings) {
+				q.resolve(settings);
+			});
+		});
+
+		return q.promise;
+	};
+
+	/**
+	 * Connects to Firebase and retrieves plugins settings for this workspace.
+	 *
+	 * @param {number} workspaceId The workspace id.
+	 *
+	 * @return {Promise<Object>} Plugin settings.
+	 */
+	srv.load = function (workspaceId) {
+		return loadPlugin().then(function (plugin) {
+			return connect(plugin, workspaceId);
+		});
+	};
+
+	/**
+	 * Saves plugins settings for this workspace.
+	 *
+	 * @param {number} workspaceId
+	 * @param {Object} settings
+	 * @param {Object} config
+	 *
+	 * @return {Promise<*>}
+	 */
+	srv.save = function (workspaceId, settings, config) {
+		if (config.$id) {
 			var index = settings.$indexFor(config.$id);
+			settings[index] = config;
+			return settings.$save(index);
+		}
 
-			if (index === -1) {
-				return $q.reject('Inexistent config');
-			}
+		return settings.$add(config);
+	};
 
-			return settings.$remove(index);
-		};
+	/**
+	 * Completely deletes a given configuration.
+	 *
+	 * @param {number} workspaceId
+	 * @param {Object} config
+	 * @param {Object} settings
+	 *
+	 * @return {Promise<*>}
+	 */
+	srv.deleteConfig = function (workspaceId, config, settings) {
+		var index = settings.$indexFor(config.$id);
 
-		return srv;
-	}]);
+		if (index === -1) {
+			return $q.reject('Inexistent config');
+		}
+
+		return settings.$remove(index);
+	};
+
+	return srv;
+}]);
