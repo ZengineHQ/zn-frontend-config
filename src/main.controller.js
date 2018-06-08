60b47843f3f6b159e3fb5912a6569e132c31b41f
Alex Weber <alexweber15@gmail.com>
2018-06-08 13:55:07 -0300

    feat: thorough settings validation to prevent runtime issues

--- src/main.controller.js
+++ src/main.controller.js
@@ -334,28 +334,92 @@ plugin.controller('wgnMultiConfigCtrl', ['$scope', '$q', '$routeParams', 'znData
 		 * @param {Object} settings
 		 */
 		function doValidateSettings(settings) {
-			// Check for required settings.
-			var required = ['title', 'pages'];
-			var keys = Object.keys(settings);
-
-			angular.forEach(required, function (option) {
-				if (keys.indexOf(option) === -1) {
-					throw new Error('Invalid multi config settings! Missing: ' + option);
-				}
-			});
+			// Check for required top level settings.
+			var pluginKeys = Object.keys(settings);
+			doValidateSettingsRequired(['title', 'pages'], pluginKeys, 'plugin');
 
 			// Make sure we have at least one page.
 			if (!Array.isArray(settings.pages) || !settings.pages.length) {
-				throw new Error('Multi config settings must have at least one page');
+				throw new Error('Invalid multi config settings! At least one page must be defined.');
 			}
 
-			// Add some default settings we don't want to be empty.
+			// Ensure no top level options exist other than the allowed ones.
+			var allowedPluginKeys = ['title', 'icon', 'help', 'multi', 'toggle', 'pages'];
+			doValidateSettingsAllowed(allowedPluginKeys, Object.keys(settings), 'page');
+
+			// Check page level settings.
+			var allowedPageKeys = ['id', 'name', 'fields'];
+			var allowedFieldKeys = ['id', 'name', 'required', 'help', 'type', 'belongsTo', 'restrict', 'placeholder'];
+
+			settings.pages.forEach(function (page) {
+				// Check for required page settings.
+				var pageKeys = Object.keys(page);
+				doValidateSettingsRequired(['fields'], pageKeys, 'page');
+
+				if (settings.pages.length > 1) {
+					doValidateSettingsRequired(['id', 'name'], pageKeys, 'page');
+				}
+
+				// Make sure we have at least one field.
+				if (!Array.isArray(page.fields) || !page.fields) {
+					throw new Error('Invalid multi config settings! At least one field must be defined for page ' + page.id);
+				}
+
+				// Ensure no options exist other than the allowed ones.
+				doValidateSettingsAllowed(allowedPageKeys, pageKeys, 'page');
+
+				// Check field level settings.
+				settings.pages.fields.forEach(function (field) {
+					// Check for required field settings.
+					var fieldKeys = Object.keys(field);
+					doValidateSettingsRequired(['id', 'name', 'type'], fieldKeys, 'field');
+
+					// Ensure no options exist other than the allowed ones.
+					doValidateSettingsAllowed(allowedFieldKeys, fieldKeys, 'field');
+				});
+			});
+
+			// Finally, add some default settings we don't want to be empty.
 			if (!settings.icon) {
 				settings.icon = 'icon-puzzle';
 			}
 			if (!settings.help) {
 				settings.help = 'This is some instructional text decribing what this plugin is and how to use it. Please customize it.';
 			}
+		}
+
+		/**
+		 * Validates required settings exist.
+		 *
+		 * @param {Array<string>} required The required keys.
+		 * @param {Array<string>} keys The keys to check.
+		 * @param {string} level The hierarchical level of the settings we're checking.
+		 *
+		 * @throws Error
+		 */
+		function doValidateSettingsRequired(required, keys, level) {
+			angular.forEach(required, function (option) {
+				if (keys.indexOf(option) === -1) {
+					throw new Error('Invalid multi config settings! Missing: "' + option + '" for ' + level);
+				}
+			});
+		}
+
+		/**
+		 * Validates only allowed settings exist.
+		 *
+		 * @param {Array<string>} allowed The allowed keys.
+		 * @param {Array<string>} keys The keys to check.
+		 * @param {string} level The hierarchical level of the settings we're checking.
+		 *
+		 * @throws Error
+		 */
+		function doValidateSettingsAllowed(allowed, keys, level) {
+			angular.forEach(keys, function (key) {
+				if (allowed.indexOf(key) === -1) {
+					throw new Error('Invalud multi config settings! Option "' + key + '" not allowed for ' + level);
+				}
+			});
 		}
 
 		/**
