31d1142e16781d500c9c059baa49c82cce1f9cee
Alex Weber <alexweber15@gmail.com>
2018-06-13 18:52:51 -0300

    feat: improve error messaging for validator

--- src/validator.service.js
+++ src/validator.service.js
@@ -5,11 +5,13 @@ plugin.service('wgnMultiConfigValidator', [function () {
 	 * Validates multi config settings.
 	 *
 	 * @param {Object} settings
+	 *
+	 * @throws Error
 	 */
 	srv.validateSettings = function (settings) {
 		// Check for required top level settings.
 		var pluginKeys = Object.keys(settings);
-		doValidateSettingsRequired(['title', 'pages'], pluginKeys, 'plugin');
+		doValidateSettingsRequired(['title', 'pages'], pluginKeys, 'plugin', 'top level');
 
 		// Make sure we have at least one page.
 		if (!Array.isArray(settings.pages) || !settings.pages.length) {
@@ -18,7 +20,7 @@ plugin.service('wgnMultiConfigValidator', [function () {
 
 		// Ensure no top level options exist other than the allowed ones.
 		var allowedPluginKeys = ['title', 'icon', 'help', 'multi', 'toggle', 'pages'];
-		doValidateSettingsAllowed(allowedPluginKeys, Object.keys(settings), 'page');
+		doValidateSettingsAllowed(allowedPluginKeys, Object.keys(settings), 'plugin', 'top level');
 
 		// Check page level settings.
 		angular.forEach(settings.pages, function (page) {
@@ -39,17 +41,19 @@ plugin.service('wgnMultiConfigValidator', [function () {
 	 *
 	 * @param {Object} page
 	 * @param {Object} settings
+	 *
+	 * @throws Error
 	 */
 	function doValidatePage (page, settings) {
 		var allowedPageKeys = ['id', 'name', 'fields'];
 
 		// Check for required page settings.
 		var pageKeys = Object.keys(page);
-		doValidateSettingsRequired(['fields'], pageKeys, 'page');
+		doValidateSettingsRequired(['fields'], pageKeys, 'page', page.name);
 
 		// An id and name are only really required if you have more than one page.
 		if (settings.pages.length > 1) {
-			doValidateSettingsRequired(['id', 'name'], pageKeys, 'page');
+			doValidateSettingsRequired(['id', 'name'], pageKeys, 'page', page.name);
 		}
 
 		// Make sure we have at least one field.
@@ -58,7 +62,7 @@ plugin.service('wgnMultiConfigValidator', [function () {
 		}
 
 		// Ensure no options exist other than the allowed ones.
-		doValidateSettingsAllowed(allowedPageKeys, pageKeys, 'page');
+		doValidateSettingsAllowed(allowedPageKeys, pageKeys, 'page', page.name);
 
 		// Check field level settings.
 		angular.forEach(page.fields, function (field) {
@@ -70,6 +74,8 @@ plugin.service('wgnMultiConfigValidator', [function () {
 	 * Validates field settings.
 	 *
 	 * @param {Object} field
+	 *
+	 * @throws Error
 	 */
 	function doValidateField (field) {
 		/*jshint maxcomplexity:6 */
@@ -78,10 +84,12 @@ plugin.service('wgnMultiConfigValidator', [function () {
 
 		// Check for required field settings.
 		var fieldKeys = Object.keys(field);
-		doValidateSettingsRequired(['id', 'name', 'type'], fieldKeys, 'field');
+		doValidateSettingsRequired(['id', 'name', 'type'], fieldKeys, 'field', field.name);
 
 		// Ensure no options exist other than the allowed ones.
-		doValidateSettingsAllowed(allowedFieldKeys, fieldKeys, 'field');
+		doValidateSettingsAllowed(allowedFieldKeys, fieldKeys, 'field', field.name);
+
+		console.log(allowedFieldKeys, fieldKeys);
 
 		// Ensure only valid field types are used.
 		if (validFieldTypes.indexOf(field.type) === -1) {
@@ -102,13 +110,14 @@ plugin.service('wgnMultiConfigValidator', [function () {
 	 * @param {Array<string>} required The required keys.
 	 * @param {Array<string>} keys The keys to check.
 	 * @param {string} level The hierarchical level of the settings we're checking.
+	 * @param {string} context Some additional context to display in the error message
 	 *
 	 * @throws Error
 	 */
-	function doValidateSettingsRequired (required, keys, level) {
+	function doValidateSettingsRequired (required, keys, level, context) {
 		angular.forEach(required, function (option) {
 			if (keys.indexOf(option) === -1) {
-				throw new Error('Invalid multi config settings! Missing: "' + option + '" for ' + level);
+				throw new Error('Invalid multi config settings! Missing: "' + option + '" for ' + level + ' "' + context + '"');
 			}
 		});
 	}
@@ -119,13 +128,14 @@ plugin.service('wgnMultiConfigValidator', [function () {
 	 * @param {Array<string>} allowed The allowed keys.
 	 * @param {Array<string>} keys The keys to check.
 	 * @param {string} level The hierarchical level of the settings we're checking.
+	 * @param {string} context Some additional context to display in the error message.
 	 *
 	 * @throws Error
 	 */
-	function doValidateSettingsAllowed (allowed, keys, level) {
+	function doValidateSettingsAllowed (allowed, keys, level, context) {
 		angular.forEach(keys, function (key) {
 			if (allowed.indexOf(key) === -1) {
-				throw new Error('Invalid multi config settings! Option "' + key + '" not allowed for ' + level);
+				throw new Error('Invalid multi config settings! Option "' + key + '" not allowed for ' + level + ' "' + context + '"');
 			}
 		});
 	}
