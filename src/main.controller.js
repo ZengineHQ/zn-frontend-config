9c5cbc9693f613fb89d2ba1ab4d72f39f92c4c06
Alex Weber <alexweber15@gmail.com>
2018-06-20 10:22:59 -0300

    feat: respect field, folder and choice input exclusivity in input options

--- src/main.controller.js
+++ src/main.controller.js
@@ -433,11 +433,14 @@ plugin.controller('wgnConfigCtrl', ['$scope', '$q', '$routeParams', 'znData', 'z
 			// Filter values used in other folder inputs.
 			if (formDef) {
 				angular.forEach(formDef.fields, function (f) {
-					if (f.type === fieldDef.type && f.id !== fieldDef.id && $scope.editing.config) {
-						if (f.type === 'choice' && f.id + '_source' in $scope.editing.config && $scope.editing.config[f.id + '_source']) {
-							filters.push($scope.editing.config[f.id + '_source']);
-						} else if (f.id in $scope.editing.config && $scope.editing.config[f.id]) {
-							filters.push($scope.editing.config[f.id]);
+					// Split into multiple if statements for legibility.
+					if (f.type === fieldDef.type && f.id !== fieldDef.id && f.type === 'choice') {
+						if (f.exclusive && $scope.editing.config) {
+							if (f.id + '_source' in $scope.editing.config && $scope.editing.config[f.id + '_source']) {
+								filters.push($scope.editing.config[f.id + '_source']);
+							} else if (f.id in $scope.editing.config && $scope.editing.config[f.id]) {
+								filters.push($scope.editing.config[f.id]);
+							}
 						}
 					}
 				});
